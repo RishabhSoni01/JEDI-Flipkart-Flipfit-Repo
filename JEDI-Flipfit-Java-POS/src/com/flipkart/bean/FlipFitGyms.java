50d284e68fbcaec62da224f94b23fc411ec5c48d
khushisrivastava1 <khushisrivastava361@gmail.com>
2024-10-09 13:47:00 +0530

    FlipFitGyms.java  file updated

--- JEDI-Flipfit-Java-POS/src/com/flipkart/bean/FlipFitGyms.java
+++ JEDI-Flipfit-Java-POS/src/com/flipkart/bean/FlipFitGyms.java
@@ -4,79 +4,23 @@ import java.util.ArrayList;
 
 public class FlipFitGyms extends FlipFitGymOwner {
     private int gymId;
+
+    public String getGymName() {
+        return gymName;
+    }
+
+    public void setGymName(String gymName) {
+        this.gymName = gymName;
+    }
+
+    public int getGymId() {
+        return gymId;
+    }
+
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
+
     private String gymName;
 
-    public boolean rquestApproval(){
-        return true;
-    }
-
-    @Override
-    public ArrayList<String> getGymLists() {
-        return super.getGymLists();
-    }
-
-    @Override
-    public void setGymLists(ArrayList<String> gymLists) {
-        super.setGymLists(gymLists);
-    }
-
-    @Override
-    public String getPanId() {
-        return super.getPanId();
-    }
-
-    @Override
-    public void setPanId(String panId) {
-        super.setPanId(panId);
-    }
-
-    @Override
-    public String getGSTNum() {
-        return super.getGSTNum();
-    }
-
-    @Override
-    public void setGSTIN(String gstNum) {
-        super.setGSTIN(gstNum);
-    }
-
-    @Override
-    public String getAadharNumber() {
-        return super.getAadharNumber();
-    }
-
-    @Override
-    public void setAadharNumber(String aadharNumber) {
-        super.setAadharNumber(aadharNumber);
-    }
-
-    @Override
-    public boolean getIsApproved() {
-        return super.getIsApproved();
-    }
-
-    @Override
-    public void setIsApproved(boolean isApproved) {
-        super.setIsApproved(isApproved);
-    }
-
-    @Override
-    public boolean addUser() {
-        return super.addUser();
-    }
-
-    @Override
-    public boolean registerGyms() {
-        return super.registerGyms();
-    }
-
-    @Override
-    public boolean addSlots() {
-        return super.addSlots();
-    }
-
-    @Override
-    public boolean viewCenters() {
-        return super.viewCenters();
-    }
 }
