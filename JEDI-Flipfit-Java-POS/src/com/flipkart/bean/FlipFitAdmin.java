6ec2dbcb732ef374652d227ef3ab93075867b2f7
RishabhSoni01 <rshbh226@gmail.com>
2024-10-09 14:44:08 +0530

    UserId related changes

--- JEDI-Flipfit-Java-POS/src/com/flipkart/bean/FlipFitAdmin.java
+++ JEDI-Flipfit-Java-POS/src/com/flipkart/bean/FlipFitAdmin.java
@@ -1,64 +1,19 @@
 package com.flipkart.bean;
 
-import java.util.List;
+/**
+ * Represents an admin user in the FlipFit system, extending FlipFitUser.
+ * This class provides a default constructor initializing admin details.
+ */
+public class FlipFitAdmin extends FlipFitUser {
 
-public class FlipFitAdmin {
-    // Property
-    public int adminId;
-
-    // Setter and Getter for adminId
-    public void setAdminID(int adminId) {
-        this.adminId = adminId;
-    }
-
-    public int getAdminID() {
-        return this.adminId;
-    }
-
-    // Methods as per UML diagram
-
-    // View list of gyms
-    public void viewGyms() {
-        // Logic to view gyms
-    }
-
-    // Approve gym request
-    public void approveGymRequest() {
-        // Logic to approve gym request
-    }
-
-    // Delete gym request
-    public void deleteGymRequest() {
-        // Logic to delete a gym request
-    }
-
-    // View pending gym requests
-    public List<String> viewPendingGymRequest() {
-        // Logic to return a list of pending gym requests
-        return null; // Placeholder return
-    }
-
-    // View pending customer requests
-    public List<String> viewPendingCustomerRequest() {
-        // Logic to return a list of pending customer requests
-        return null; // Placeholder return
-    }
-
-    // Delete gyms from a list
-    public boolean deleteGyms(List<String> gyms) {
-        // Logic to delete gyms
-        return true; // Placeholder return
-    }
-
-    // View customers
-    public void viewCustomers() {
-        // Logic to view customers
-    }
-
-    // Delete customers from a list
-    public boolean deleteCustomers(List<String> customers) {
-        // Logic to delete customers
-        return true; // Placeholder return
+    /**
+     * Default constructor for creating an admin user.
+     * Initializes with default values for admin user ID, username, email, password,
+     * and assigns the role as ADMIN.
+     */
+    public FlipFitAdmin() {
+        super(101, "admin", "admin@flipfit.com", "8812324234", "password","Bengaluru",4798263,FlipFitRole.ADMIN);
     }
 }
+
 
