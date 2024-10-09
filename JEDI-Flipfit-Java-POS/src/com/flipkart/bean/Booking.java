01061a630e41ed45664e10c363244a34dc80ef2d
sreelakshmi.a <sreelakshmi.a@fk1089-226111l.fkipl.flipkart.com>
2024-10-09 14:41:10 +0530

    extra dependencies removed

--- JEDI-Flipfit-Java-POS/src/com/flipkart/bean/Booking.java
+++ JEDI-Flipfit-Java-POS/src/com/flipkart/bean/Booking.java
@@ -4,18 +4,10 @@ import java.time.LocalDateTime;
 // Represents a booking made by a user at a gym center.
 
 public class Booking {
-    // Unique identifier of the user making the booking.
-    private Integer customerId;
 
     // Unique identifier for this booking.
     private Integer bookingID;
-
-    // The gym center where the booking is made.
-    private Integer gymID;
-
-    // The start time of the booking.
-    private Integer slotID;
-
+    private LocalDateTime bookingDate;
     public LocalDateTime getBookingDate() {
         return bookingDate;
     }
@@ -25,38 +17,25 @@ public class Booking {
     }
 
 
-    private LocalDateTime bookingDate;
+
     /* Constructor to initialize a Booking object.
     Parameters:
-       userID: ID of the user making the booking.
-       bookingID: Unique ID for this booking.
+
        gymCenter: GymCenter object representing the gym where booking is made.
        starttime: LocalDateTime object representing the start time of the booking.
      */
-    public Booking(Integer userID, Integer bookingID, Integer gymID, Integer slotID, LocalDateTime bookingDate) {
-        this.customerId = userID;
+    public Booking(Integer bookingID, LocalDateTime bookingDate) {
         this.bookingID = bookingID;
-        this.gymID = gymID;
-        this.slotID = slotID;
         this.bookingDate = bookingDate;
     }
 
 
     // Getter for userID.
-    public Integer getUserID() {
-        return customerId;
-    }
 
     // Getter for BookingID.
     public Integer getBookingID() {
         return bookingID;
     }
     // Getter for gymCenter.
-    public Integer getGymID() {
-        return gymID; }
-
-
-    public Integer getSlotID() { return slotID; }
-
 
 }
