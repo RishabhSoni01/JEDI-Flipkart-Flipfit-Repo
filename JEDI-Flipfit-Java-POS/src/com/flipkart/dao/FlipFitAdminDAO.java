740409c2ed83c215b52718be35cc09e6aa1db412
khushisrivastava1 <khushisrivastava361@gmail.com>
2024-10-14 10:19:34 +0530

    Adding comments

--- JEDI-Flipfit-Java-POS/src/com/flipkart/dao/FlipFitAdminDAO.java
+++ JEDI-Flipfit-Java-POS/src/com/flipkart/dao/FlipFitAdminDAO.java
@@ -1,24 +1,79 @@
 package com.flipkart.dao;
+
 import java.util.List;
 import com.flipkart.bean.*;
 
+/**
+ * Interface for administrative operations related to gym owners, gyms, and customers.
+ * Provides methods to approve requests and retrieve information about pending and approved entities.
+ */
 public interface FlipFitAdminDAO {
-    // Function to improve the gymOwner request
-    public boolean approveGymOwner (String gymOwnerId);
-    // Function to improve the Customer
-    public boolean approveCustomer (String customerId);
-    // Function to list out the pending gym owners
+
+    /**
+     * Approves a gym owner request by updating their status in the database.
+     *
+     * @param gymOwnerId the ID of the gym owner to be approved.
+     * @return true if the approval was successful, false otherwise.
+     */
+    public boolean approveGymOwner(String gymOwnerId);
+
+    /**
+     * Approves a customer request by updating their status in the database.
+     *
+     * @param customerId the ID of the customer to be approved.
+     * @return true if the approval was successful, false otherwise.
+     */
+    public boolean approveCustomer(String customerId);
+
+    /**
+     * Retrieves a list of all pending gym owner requests.
+     *
+     * @return a list of FlipFitGymOwner objects representing gym owners awaiting approval.
+     */
     public List<FlipFitGymOwner> viewPendingGymOwners();
-    // Function to list out the pending gyms
+
+    /**
+     * Retrieves a list of all pending gym requests.
+     *
+     * @return a list of FlipFitGyms objects representing gyms awaiting approval.
+     */
     public List<FlipFitGyms> viewPendingGyms();
-    // Function to approve a Gym request
+
+    /**
+     * Approves a gym request by updating its status in the database.
+     *
+     * @param gymId the ID of the gym to be approved.
+     * @return true if the approval was successful, false otherwise.
+     */
     public boolean approveGym(String gymId);
-    // Function to list out all the gyms
+
+    /**
+     * Retrieves a list of all gyms in the system.
+     *
+     * @return a list of FlipFitGyms objects representing all gyms.
+     */
     public List<FlipFitGyms> viewGyms();
-    // Function to list out all the gym owners
+
+    /**
+     * Retrieves a list of all gym owners.
+     *
+     * @param gymOwnerId the ID of the gym owner (not used in this method; consider removing).
+     * @return a list of FlipFitGyms objects associated with the gym owners.
+     */
     public List<FlipFitGyms> viewGymsOwner(int gymOwnerId);
-    // Function to list out all the customers
+
+    /**
+     * Retrieves a list of all customers in the system.
+     *
+     * @return a list of FlipFitCustomer objects representing all customers.
+     */
     public List<FlipFitCustomer> getAllCustomers();
-    // Function to list all the pending customers
+
+    /**
+     * Retrieves a list of all pending customer requests.
+     *
+     * @return a list of FlipFitGyms objects representing pending customer requests.
+     */
     public List<FlipFitGyms> getPendingCustomers();
 }
+
